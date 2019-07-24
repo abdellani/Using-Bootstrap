ace249d979acdc85eabe124664a551683bbe04f7
abdellani <abdellani@gmail.com>
2019-07-24 23:47:55 +0100

    Implement part1 with basic responsiveness

--- js/javascript.js
+++ js/javascript.js
@@ -2,19 +2,20 @@ window.onscroll=scroll
 window.onresize=scroll
 
 let headerNavbar=document.getElementById("header-navbar")
-headerNavbarPosition=headerNavbar.offsetTop
-
 let headerDiv=document.getElementById("header-div")
+let main = document.getElementsByTagName("main")[0]
+headerNavbarPosition=headerNavbar.offsetTop
 scroll();
 
 function scroll(){
   if( window.pageYOffset>headerNavbarPosition || document.documentElement.clientWidth<1000)
   {
+    let mainTopMargin=Number(main.offsetTop)
     headerDiv.classList.add("fixed-top")
     headerDiv.classList.add("header-scrolled-down")
-
     headerNavbar.classList.add("navbar-scrolled-down")
     headerNavbar.classList.remove("navbar-expand-lg")
+    main.style.marginTop=mainTopMargin+"px"
   }
   else{
      headerDiv.classList.remove("fixed-top")
@@ -22,5 +23,6 @@ function scroll(){
 
      headerNavbar.classList.remove("navbar-scrolled-down")
      headerNavbar.classList.add("navbar-expand-lg")
+     main.style.marginTop="0"
     }
 }

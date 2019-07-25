c83e1e8260e1ec1ce92159d7b15a494dae226b05
abdellani <abdellani@gmail.com>
2019-07-25 18:00:57 +0100

    Implement part-1 with responesiveness

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
 window.onscroll = scroll
 window.onresize = scroll
 
+let header = document.getElementById("header")
 let headerNavbar = document.getElementById("header-navbar")
 let headerDiv = document.getElementById("header-div")
 let main = document.getElementsByTagName("main")[0]
@@ -11,26 +12,41 @@ headerNavbarPosition = headerNavbar.offsetTop
 
 scroll();
 
+function toggleNavBar() {
+  let mainTopMargin = Number(main.offsetTop);
+  headerDiv.classList.add("fixed-top")
+  headerDiv.classList.add("header-scrolled-down")
+  headerNavbar.classList.add("navbar-scrolled-down")
+  headerNavbar.classList.remove("navbar-expand-lg")
+  main.scrollIntoView()
+  showHideNavbar()
+ 
+}
+function expandNavBar() {
+  headerDiv.classList.remove("fixed-top")
+  headerDiv.classList.remove("header-scrolled-down")
+
+  headerNavbar.classList.remove("navbar-scrolled-down")
+  headerNavbar.classList.add("navbar-expand-lg")
+  main.style.marginTop = "0"
+  showNavbar()
+
+}
 function scroll() {
-  if (window.pageYOffset > headerNavbarPosition || document.documentElement.clientWidth < 1000) {
-    let mainTopMargin = Number(main.offsetTop)
-    headerDiv.classList.add("fixed-top")
-    headerDiv.classList.add("header-scrolled-down")
-    headerNavbar.classList.add("navbar-scrolled-down")
-    headerNavbar.classList.remove("navbar-expand-lg")
-    main.style.marginTop = mainTopMargin + "px"
-    showHideNavbar()
+  if (window.pageYOffset > headerNavbarPosition || document.documentElement.clientWidth <= 992) {
+    if (!headerDiv.classList.contains("fixed-top"))
+      toggleNavBar()
+    if (document.documentElement.clientWidth <= 992) {
+      main.style.marginTop = 50 + "px"
+    } else {
+      main.style.marginTop = "160px"
+    }
+
   }
   else {
-    headerDiv.classList.remove("fixed-top")
-    headerDiv.classList.remove("header-scrolled-down")
-
-    headerNavbar.classList.remove("navbar-scrolled-down")
-    headerNavbar.classList.add("navbar-expand-lg")
-    main.style.marginTop = "0"
-    showNavbar()
+    if (headerDiv.classList.contains("fixed-top"))
+      expandNavBar()
   }
-
 }
 
 function updateToggleNavbarButton() {
